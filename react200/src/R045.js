f7245ebe4529a945f27eed5a2022d47d39c1b728
knsnyo <isc963@naver.com>
2021-12-14 15:47:15 +0900

    0211214 15:50

---
+++ react200/src/R045.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { InputGroup, InputGroupText, Input, Button } from 'reactstrap';
+
+function R045(props){
+	return(
+		<>
+		  <InputGroup>
+			  <Input placeholder='user ID'/>
+				<InputGroupText>@reactmail.com</InputGroupText>
+			</InputGroup>
+			<InputGroup>
+				<Button>BUTTON</Button>
+				<Input/>
+			</InputGroup>
+		</>
+	)
+}
+
+export default R045;
