786326ac4eaa8b9fa0cdf0e649616eb2b81baddd
knsnyo <isc963@naver.com>
2021-12-16 15:32:14 +0900

    2021116 15:35

---
+++ react200/src/R046.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import{Button} from 'reactstrap';
+
+function R046(props){
+	return(
+		<div style={{backgroundColor: '#eeeeee'}}>
+			<h1 className='display-4'>React 200</h1>
+			<p className='h4'>Hi</p>
+			<hr className='my-2'/>
+			<p>Bye</p>
+			<p className='lead'>
+				<Button color='danger'>Go detail</Button>
+			</p>
+		</div>
+	)
+}
+
+export default R046;
+
+/* Jumbotron 없어졌네? */
